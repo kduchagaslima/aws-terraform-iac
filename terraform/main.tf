837d931678acc996b50a0c504969a25e05c8b37f
CARLOS EDUARDO LIMA <61298521+kduchagaslima@users.noreply.github.com>
2022-06-20 18:09:29 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,10 @@ provider "aws" {
 resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
+   tags = {
+    Name = minha_vpc
+   }  
+  
 }
 # Cria um Internet Gateway que possibilita a comunicação do VPN com o mundo externo
 resource "aws_internet_gateway" "default" {
